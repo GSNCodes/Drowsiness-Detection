db2ba70b33e00d9d25d64621a91ab5a9c21de28b
GSNCodes <37922208+GSNCodes@users.noreply.github.com>
2019-11-23 17:14:00 +0530

    Update drowsiness detection.py

--- drowsiness detection.py
+++ drowsiness detection.py
@@ -9,9 +9,9 @@ import time
 mixer.init()
 sound = mixer.Sound('alarm.wav')
 
-face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
-leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
-reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+face = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+leye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
+reye = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
 
 
 
